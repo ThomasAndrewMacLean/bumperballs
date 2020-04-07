508770d178d9f075b50bfc92a33bc7ae000ea5a2
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-04-07 23:27:17 +0200

    global css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import App from 'next/app';
 import React, { createContext } from 'react';
 
+require('../styles/style.css');
+
 import Airtable from 'airtable';
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
   process.env.AIRTABLE_APP
--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,51 @@ import T from '../components/Translation';
 
 const IndexPage = () => {
   return (
-    <div>
-      <Title>My First Next.js Page</Title>
-      <T id={'watZijnBumperballen'}></T>
-    </div>
+    <Main>
+      <Hero>
+        <Title>BMS Rent</Title>
+        <T id={'watZijnBumperballen'}></T>
+      </Hero>
+      <Div>
+        <T id={'waarGebruiken'}></T>
+      </Div>
+      <Div>
+        <T id={'voorWelkeGelegenheden'}></T>
+      </Div>
+      <Div>
+        <T id={'welkeSpelletjes'}></T>
+      </Div>
+      <Div>
+        <T id={'veiligheid'}></T>
+      </Div>
+      <Div>
+        <T id={'praktischeInfo'}></T>
+      </Div>
+      <Div>
+        <T id={'waarborg'}></T>
+      </Div>
+      <Div>
+        <T id={'formules'}></T>
+      </Div>
+    </Main>
   );
 };
 
+const Main = styled.main`
+  margin: auto;
+  max-width: 800px;
+  background: lightblue;
+`;
+const Hero = styled.div`
+  padding: 2rem 4rem;
+  background: green;
+`;
 const Title = styled.h1`
-  color: red;
+  /* color: red; */
+`;
+
+const Div = styled.div`
+  padding: 0 4rem;
 `;
 
 export default IndexPage;
