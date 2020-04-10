1c5eb101767f35f18f4832ea8688e915df78ec71
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-04-10 14:11:44 +0200

    add prefix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 //const withCSS = require('@zeit/next-css');
 
+const isProduction = process.env.NODE_ENV === 'production';
+const prefix = isProduction ? '/bumperballs' : '';
+
 const envVariables = {
   AIRTABLE_APP: process.env.AIRTABLE_APP,
   AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+  ASSET_PREFIX: prefix,
 };
 
 module.exports = {
